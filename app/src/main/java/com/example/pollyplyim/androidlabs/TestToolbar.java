5a9d4126134ad1e1b6685e029420c742d2b17d28
pollypolingyim <>
2018-04-02 13:02:11 -0400

    lab 8 commit

--- app/src/main/java/com/example/pollyplyim/androidlabs/TestToolbar.java
+++ app/src/main/java/com/example/pollyplyim/androidlabs/TestToolbar.java
@@ -1,18 +1,39 @@
 package com.example.pollyplyim.androidlabs;
 
-import android.os.Bundle;
-import android.app.Activity;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class TestToolbar extends Activity {
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class TestToolbar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Version 1.0, by Polly Yim", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     public boolean onCreateOptionsMenu (Menu m) {
@@ -25,11 +46,45 @@ public class TestToolbar extends Activity {
         switch(id){
             case R.id.action_one:
                 Log.d("Toolbar", "Option 1 selected");
+                Snackbar.make(getCurrentFocus(), "You selected item 1", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
                 break;
             case R.id.action_two:
-                //Start an activity…
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(R.string.toolbar_title);
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK button
+                        finish();
+                    }
+                });
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
                 break;
             case R.id.action_three:
+                AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
+                LayoutInflater inflater = this.getLayoutInflater();
+                builder2.setView(inflater.inflate(R.layout.dialog_layout, null))
+                        // Add action buttons
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                EditText edt = (EditText)findViewById(R.id.snackbar_msg);
+                                Snackbar.setText(edt.getText().toString());
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+
                 break;
             case R.id.action_four:
                 Toast.makeText(getApplicationContext(),
