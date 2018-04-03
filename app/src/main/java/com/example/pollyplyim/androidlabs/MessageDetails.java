69b39fc0505af8b18245c0fa3fae184836c1b811
pollypolingyim <pollypolingyim@gmail.com>
2018-04-03 19:37:47 -0400

    Update MessageDetails.java

--- app/src/main/java/com/example/pollyplyim/androidlabs/MessageDetails.java
+++ app/src/main/java/com/example/pollyplyim/androidlabs/MessageDetails.java
@@ -20,20 +20,12 @@ public class MessageDetails extends Activity {
         long id = getIntent().getLongExtra("ID", 0);
         String message = getIntent().getStringExtra("Message");
 
-//        boolean isTablet = getResources().getBoolean(R.bool.isTablet);
-
-//        if(!isTablet) {
         fragmentTransaction = getFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, MessageFragment.newInstance(id, message))
                 .commit();
-//        }
-//        else{
-//            startActivity(new Intent(this, MessageDetails.class), savedInstanceState);
-//        }
     }
 
     public void deleteMessageWithId(long id) {
-        //did I implement it correctly?
         Intent data = new Intent();
         data.putExtra("ID", id);
         setResult(40, data);
